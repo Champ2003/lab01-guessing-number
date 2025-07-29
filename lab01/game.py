4733f7a02e25bce3ef5849d11975d193e428b3a3
Champ2003 <lowthanongsak_c@silpakorn.edu>
2025-07-29 11:33:12 +0700

    Update game.py

--- lab01/game.py
+++ lab01/game.py
@@ -31,6 +31,13 @@ def check_guess(secret_number, player_guess):
     4. If the guess is too low, return the string "low".
     """
     # Student 2: Add your code here
+    if player_guess == secret_number:
+        return "correct"
+    elif player_guess > secret_number:
+        return "high"
+    else:
+        return "low"
+    import random
     pass
 
 def play_game():
@@ -46,18 +53,18 @@ def play_game():
     # secret_number = random.randint(1, 100)
     # attempts = 0
     #
-    # while True:
-    #     attempts += 1
-    #     guess = get_player_guess()
-    #     result = check_guess(secret_number, guess)
-    #
-    #     if result == "correct":
-    #         print(f"Congratulations! You guessed the number in {attempts} attempts.")
-    #         break
-    #     elif result == "high":
-    #         print("Too high! Try again.")
-    #     elif result == "low":
-    #         print("Too low! Try again.")
+     while True:
+         attempts += 1
+         guess = get_player_guess()
+         result = check_guess(secret_number, guess)
+    
+         if result == "correct":
+             print(f"Congratulations! You guessed the number in {attempts} attempts.")
+             break
+         elif result == "high":
+             print("Too high! Try again.")
+         elif result == "low":
+             print("Too low! Try again.")
 
 if __name__ == "__main__":
     play_game()
